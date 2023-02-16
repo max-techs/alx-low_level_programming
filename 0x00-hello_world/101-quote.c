f934041165b747510999ca1d401043954ca0e901
max-techs <samueljohnmaxi@gmail.com>
2023-02-16 15:45:56 +0100

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-  * main - Uses write to print
-  * Return: Always 0 means success
+  * main - prints exactly a piece of art is useful
+  *:x
+  *Return: Always 0 means success
   */
 int main(void)
 {
-	write(2," and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
