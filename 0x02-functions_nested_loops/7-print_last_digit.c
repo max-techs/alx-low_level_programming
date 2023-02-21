472fb904ea27e34987593a331fd09906c062ba3f
max-techs <samueljohnmaxi@gmail.com>
2023-02-21 09:37:43 -0800

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,17 @@
 int print_last_digit(int n)
 {
 	int last_digit;
-	last_digit = n % 10;
 
+	if (n < 0)
+	{
+		last_digit = (-1 * (n % 10));
+		_putchar (last_digit + '0');
+		return (last_digit);
+	}
+	else
+	{
+	last_digit = (n % 10);
+	_putchar (last_digit + '0');
 	return (last_digit);
+	}
 }
