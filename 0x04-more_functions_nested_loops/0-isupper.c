146250ebd190c02154b3a1140fdca1c2435895de
max-techs <samueljohnmaxi@gmail.com>
2023-02-23 19:09:08 +0100

    0-isuper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,12 @@
 
 /**
   * _isupper - checks for uppercase character.
-  *
+  * @c: character to check
   * Return: 1 if upper or 0 if otherwise
   */
 int _isupper(int c)
 {
-	if ( c == 'A' || c == 'Z')
+	if (c == 'A' || c == 'Z')
 	{
 		return (1);
 	}
